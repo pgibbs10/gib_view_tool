1552887f823182c45fd2f1a8b37c7f8c12ec24c9
Paul Gibbs <pwgibbs10@gmail.com>
2017-12-12 09:58:26 -0700

    Dev tool initial commit

---
+++ lib/gib_view_tool.rb
@@ -0,0 +1,6 @@
+require "gib_view_tool/version"
+require "gib_view_tool/renderer"
+
+module GibViewTool
+  # Your code goes here...
+end
---
+++ lib/gib_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GibViewTool
+  VERSION = "0.1.0"
+end
